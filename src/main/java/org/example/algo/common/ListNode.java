7867a903affd67318da57529e935e4275184ded7
Xiaohu <yelihu.ylh@alibaba-inc.com>
2024-01-02 21:18:42 +0800

    feat: 首次提交

---
+++ src/main/java/org/example/algo/common/ListNode.java
@@ -0,0 +1,12 @@
+package org.example.algo.common;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author yelihu
+ */
+@AllArgsConstructor
+public class ListNode {
+    int val;
+    ListNode next;
+}
---
+++ src/main/java/org/example/algo/common/TreeNode.java
@@ -0,0 +1,13 @@
+package org.example.algo.common;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author yelihu
+ */
+@AllArgsConstructor
+public class TreeNode {
+    int val;
+    ListNode left;
+    ListNode right;
+}
